effee13e6ab755fe07e01cbe648a89d9dcda7d70
Jose <arunx.jose@intel.com>
2020-11-16 18:00:30 +0530

    Updates

---
+++ precache-manifest.b3d077d05db19a9644d22d6717bce0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d611c33940a98d88d75e09aea0e84a",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "49db5c5981ebe505cd5c",
+    "url": "/react-monsters/static/css/main.a5dde09b.chunk.css"
+  },
+  {
+    "revision": "71a781b61d5554e69f5a",
+    "url": "/react-monsters/static/js/2.9411a8ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.9411a8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49db5c5981ebe505cd5c",
+    "url": "/react-monsters/static/js/main.670e7dd6.chunk.js"
+  },
+  {
+    "revision": "e163a836d5dd22b5ea00",
+    "url": "/react-monsters/static/js/runtime-main.00db60b7.js"
+  }
+]);
